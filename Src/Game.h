81ec6a219ddf0a0955a5d8fd86eee38cc36030a8
Binh Minh <goldie2k3@gmail.com>
2022-09-30 14:42:24 +0700

    'init_Aliens'

--- Src/Game.h
+++ Src/Game.h
@@ -1,6 +1,14 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
+#define alienRows 8 //So dong quai
+#define alienColumns 6 //So cot quai 
+int alienHorzSpacing = 150; //Khoang cach giua cac hang quai
+int alienVertSpacing = 35; // Khoang cach giua cac cot quai
+int alienTopOffset = 40; //Khoang cach giua quai voi thanh man hinh tren 
+
+int alienMoveFrameCounter=0;
+
 class Game {
 	public:
 		void run();

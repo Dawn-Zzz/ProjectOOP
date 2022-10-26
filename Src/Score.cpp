af84836d79709b608d2d6b378617ee301c15d03d
Binh Minh <goldie2k3@gmail.com>
2022-10-26 23:38:28 +0700

    add_and_fixed_score

--- Src/Score.cpp
+++ Src/Score.cpp
@@ -5,15 +5,13 @@
 #include "Variable.h"
 
 Score::Score(){
-	Num = 5;
 	//readFile();
 	currentScore = 0;
 	for (int i = 0; i < Num; i++) {
-		HighScore[i] = 0;
+		infor[i].Name="NoName";
+		infor[i].HighScore = 0;
 	}
-//	writeFile();
-	
-	//printScores();
+	//writeFile();
 }
 
 void Score::setCurrentScore(){
@@ -25,44 +23,47 @@ int Score::getCurrentScore(){
 }
 
 void Score::UpdateHighScore(){
-	//int s = currentScore;
-	for (int i = 0; i < Num; i++) {
-		if (currentScore > HighScore[i]) {
-			std::swap(currentScore, HighScore[i]);
+	for (int i=0;i<Num-1;i++)
+		for (int j=Num-1;j>i;j--) {
+			if (infor[j].HighScore>infor[j-1].HighScore){
+				Information temp = infor[j];
+				infor[j] =infor[j-1];
+				infor[j-1]=temp;	
+			}
 		}
-	}
-	std::sort(HighScore, HighScore + Num, [](int a, int b) { return a > b; });
+}
+
+void Score::setLast(string name) {
+	infor[Num-1].HighScore=score;
+	infor[Num-1].Name=name;
 }
 
 int Score::getBestScore(){
-	return HighScore[0];
+	return infor[0].HighScore;
+}
+
+int Score::getLastScore() {
+	return infor[Num-1].HighScore;
 }
 
 void Score::readFile(){
-	std::fstream input;
-	input.open("HighScore.txt", std::ios::binary | std::ios::in);
-	input.read((char*)HighScore, sizeof(HighScore));
+	std::ifstream input;
+	input.open("HighScore.txt");
+	for (int i=0; i<Num; i++) {
+		input>>infor[i].HighScore;
+		getline(input,infor[i].Name);
+	}
 	input.close();
 }
 
 void Score::writeFile(){
-	std::fstream output;
-	output.open("HighScore.txt", std::ios::binary | std::ios::out);
-	output.write((char*)HighScore, sizeof(HighScore));
+	std::ofstream output;
+	output.open("HighScore.txt");
+	for (int i=0; i<Num; i++) {
+		output<<infor[i].HighScore;
+		output<<infor[i].Name<<endl;
+	}
 	output.close();
 }
 
-int* Score::getHighScore(){
-	return HighScore;
-}
 
-int Score::getNum(){
-	return Num;
-}
-
-void Score::printScores(){
-	for (int i = 0; i < Num; i++) {
-		printf("%d ", HighScore[i]);
-	}
-	printf("\n");
-}
